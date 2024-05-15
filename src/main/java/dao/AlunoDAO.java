4d351370dbdebfbf37efbc633cfff5c36fa51d70
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2024-05-15 10:42:27 -0300

    add: Documentação de atributos, métodos e classes.

--- src/main/java/dao/AlunoDAO.java
+++ src/main/java/dao/AlunoDAO.java
@@ -7,17 +7,35 @@ import modelo.Aluno;
  *  Aqui vamos simular a persistência de dados.
  */
 public class AlunoDAO {
-
+    
+    /**
+     * ArrayList que simula a persistência dos dados.
+     */
     public static ArrayList<Aluno> minhaLista = new ArrayList<>();
 
+    /**
+     * Retorna o ArrayList.
+     * 
+     * @return O ArrayList.
+     */
     public static ArrayList<Aluno> getMinhaLista() {
         return minhaLista;
     }
 
-    public static void setMinhaLista(ArrayList<Aluno> MinhaLista) {
-        AlunoDAO.minhaLista = MinhaLista;
+    /**
+     * Modificada o ArrayList.
+     * 
+     * @param minhaLista Um ArrayList.
+     */
+    public static void setMinhaLista(ArrayList<Aluno> minhaLista) {
+        AlunoDAO.minhaLista = minhaLista;
     }
 
+    /**
+     * Retorna o maior id do aluno do ArrayList.
+     * 
+     * @return Um id com o maior id de aluno.
+     */
     public static int maiorID() {        
         int maiorID = 0;
         for (int i = 0; i < minhaLista.size(); i++) {
--- src/main/java/modelo/Aluno.java
+++ src/main/java/modelo/Aluno.java
@@ -8,17 +8,30 @@ import java.util.ArrayList;
  */
 public class Aluno extends Pessoa {
 
-    // Atributos
+    /**
+     * Curso que o aluno realiza.
+     */
     private String curso;
+    /**
+     * Fase que o aluno se encontra.
+     */
     private int fase;
 
-    // Construtor de Objeto Vazio
+    /**
+     * Construtor de Objeto Vazio.
+     */
     public Aluno() {
         this(0,"",0,"",0);
     }
-
+    
     /**
-     * Construtor com parâmetro
+     * Construtor com parâmetro.
+     * 
+     * @param id Identificador do aluno.
+     * @param nome Nome do aluno.
+     * @param idade Idade do aluno.
+     * @param curso Curso do aluno.
+     * @param fase Fase do aluno.
      */
     public Aluno( int id, String nome, int idade, String curso, int fase) {
         super(id, nome, idade);
@@ -27,22 +40,47 @@ public class Aluno extends Pessoa {
     }
 
     // Métodos GET e SET
+    /**
+     * Retorna o curso do aluno.
+     * 
+     * @return Uma String com o nome do curso do aluno.
+     */
     public String getCurso() {
         return curso;
     }
 
+    /**
+     * Modifica o curso do aluno.
+     * 
+     * @param curso Uma string com o nome do curso do aluno.
+     */
     public void setCurso(String curso) {
         this.curso = curso;
     }
 
+    /**
+     * Retorna a fase do aluno.
+     * 
+     * @return  Um inteiro com a fase do aluno.
+     */
     public int getFase() {
         return fase;
     }
 
+    /**
+     * Modifica a fase do aluno.
+     * 
+     * @param fase Um int com a fase do aluno.
+     */
     public void setFase(int fase) {
         this.fase = fase;
     }
 
+    /**
+     * Retorna os dados do aluno em uma string.
+     * 
+     * @return Uma string com todos os dados do aluno concatenado.
+     */
     @Override
     public String toString() {
         return super.toString() + "curso=" + curso + ", fase=" + fase;
@@ -50,17 +88,24 @@ public class Aluno extends Pessoa {
  
     /*  ABAIXO OS MéTODOS PARA USO JUNTO COM O DAO
         SIMULANDO A ESTRUTURA EM CAMADAS PARA USAR COM BANCOS DE DADOS.
-    */
-    
+    */    
     /**
-     * Retorna a Lista de Alunos(objetos)
+     * Retorna a Lista de Alunos(objetos).
+     * 
+     * @return Um ArrayList com todos os Alunos.
      */
     public ArrayList<Aluno> getMinhaLista() {
         return AlunoDAO.getMinhaLista();
     }
 
     /**
-     * Cadastra novo aluno
+     * Insere um novo aluno.
+     * 
+     * @param nome O nome do aluno.
+     * @param idade A idade do aluno.
+     * @param curso O curso do aluno
+     * @param fase A fase do aluno.
+     * @return Verdadeiro ou falso se conseguiu fazer a inclusão.
      */
     public boolean insertAlunoBD(String nome, int idade, String curso, int fase) {
         int id = this.maiorID() + 1;
@@ -70,7 +115,10 @@ public class Aluno extends Pessoa {
     }
 
     /**
-     * Deleta um aluno especÍfico pelo seu campo ID
+     * Deleta um aluno especÍfico pelo seu ID.
+     * 
+     * @param id Id do aluno a ser excluído.
+     * @return Verdadeiro ou falso se conseguiu fazer a exclusão.
      */
     public boolean deleteAlunoBD(int id) {
         int indice = this.procuraIndice(id);
@@ -79,7 +127,14 @@ public class Aluno extends Pessoa {
     }
 
     /**
-     * Edita um aluno especÍfico pelo seu campo ID
+     * Edita um aluno especÍfico pelo seu ID.
+     * 
+     * @param id O id do aluno.
+     * @param nome O nome do aluno.
+     * @param idade A idade do aluno.
+     * @param curso O curso do aluno
+     * @param fase A fase do aluno.
+     * @return Verdadeiro ou falso se conseguiu fazer a inclusão.
      */
     public boolean updateAlunoBD(int id, String nome, int idade, String curso, int fase) {
         Aluno objeto = new Aluno(id, nome, idade, curso, fase);
@@ -87,9 +142,12 @@ public class Aluno extends Pessoa {
         AlunoDAO.minhaLista.set(indice, objeto);
         return true;
     }
-
+    
     /**
      * Procura o INDICE de objeto da MinhaLista que contem o ID enviado.
+     * 
+     * @param id O Id do aluno a ser procurado.
+     * @return Um inteiro com a posição do aluno no ArrayList.
      */
     private int procuraIndice(int id) {
         int indice = -1;
@@ -102,7 +160,10 @@ public class Aluno extends Pessoa {
     }
     
     /**
-     * Carrega dados de um aluno especÍfico pelo seu ID
+     * Carrega dados de um aluno especÍfico pelo seu ID.
+     * 
+     * @param id O id do aluno a ser carregado.
+     * @return Um objeto aluno preenchido.
      */
     public Aluno carregaAluno(int id) {
         int indice = this.procuraIndice(id);
@@ -110,7 +171,9 @@ public class Aluno extends Pessoa {
     }
     
     /**
-     * Retorna o maior ID da nossa base de dados
+     * Retorna o maior ID da nossa base de dados.
+     * 
+     * @return Um inteiro com o maior valor de Id de aluno.
      */
     public int maiorID(){
         return AlunoDAO.maiorID();
